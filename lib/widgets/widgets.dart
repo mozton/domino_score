0e61c60dd5668fd5c4d25bbf82ccd91483fd79fa
mozton <zamirfx3@gmail.com>
2025-11-12 09:17:26 -0400

    icon_domino

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,9 +1,6 @@
 export 'package:dominos_score/widgets/icon_domino_5-1.dart';
-
 export 'package:dominos_score/widgets/button_add_team_and_score_v1.dart';
-
 export 'package:dominos_score/widgets/score_list_v1.dart';
-
 export 'package:dominos_score/widgets/card_team_v1.dart';
 export 'package:dominos_score/widgets/score_view.dart';
 export 'package:dominos_score/widgets/switch_theme.dart';
